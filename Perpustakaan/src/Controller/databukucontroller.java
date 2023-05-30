5e1335daab17dd24da51b84427d99fd7a2dfaa9c
Rayhan Fairuz Sakha <64479419+RayhanSakha@users.noreply.github.com>
2023-05-30 12:41:51 +0700

    Add files via upload

---
+++ Perpustakaan/src/Controller/databukucontroller.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+import java.util.List;
+import DAOdatabuku.databukuDAO;
+import DAOImplement.implementDAO;
+import model.*;
+import view.MainView;
+/**
+ *
+ * @author PC PRAKTIKUM
+ */
+public class databukucontroller {
+    MainView frame;
+    implementDAO implbuku;
+    
+}
